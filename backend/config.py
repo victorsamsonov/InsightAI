d75956c486fb4a9bf26999f5dcdfc299d9e7b5c3
Victor <vsamsonov@hawk.iit.edu>
2023-09-18 13:26:32 -0500

    config

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GOOGLE_CREDENTIALS_PATH = "./insightai-399303-c89d194be4b9.json"
-OPENAI_API_KEY = "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"
